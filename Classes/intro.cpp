8827813686b8e59b143c3390ece42a4657791518
Mahek <mmahekk24@gmail.com>
2023-08-08 20:08:03 +0530

    kk

---
+++ Classes/intro.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+//blueprint or template
+// once a template (classes) is made, is can be used to replicate any number of objects. The behavior (methods and attributes: properties) remains same for all objects.
+
+//Attributes:
+//class car()
+//1. color
+//2. weight
+//3. speed
+
+//Behaviour:
+//acceleration(){
+
+//}
+
+int main()
+{
+
+}
