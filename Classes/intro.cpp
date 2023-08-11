0b09498938aef11bede247d23af1897f3ab46a33
Mahek <mmahekk24@gmail.com>
2023-08-11 19:10:26 +0530

    Data Structures And ALgoritm

--- Classes/intro.cpp
+++ Classes/intro.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//blueprint or template
-// it has two things: 1. Attributes and 2. Behavior
-//Attributes:
-//It generally means a Feature i.e. something which is special about it.
-//Behavior means how it acts or interacts with other things and functions present beside it.
-//for every classes, these two can be defined and when the classes are used, these things will remain same despite of the size and use. that is why it is called as blueprint i.e. litrally follow what ever is mentioned. it is template on which the user just needs to use it for covinience and save time.
-// here the user is objects i.e. real world entities which will use these classes and the task of programmer will be reduced drastically as she will not have to define attributes and behavior every time when a new object comes into existence
-
+// blueprint or template
+// 1. it has two things: 1. Attributes and 2. Behavior
+// Attributes:
+// 2. It generally means a Features or properties i.e. something which is special and different about it.
+// 3. Behavior means how it acts or interacts with other things and functions present beside / along with it.
+// 4. for every classes, these two can be defined and when the classes are used i.e. objects come into existence, these things will remain same despite of the size and use. that is why it is called as blueprint i.e. litrally follow what ever is mentioned. it is template on which the user/objects just needs to use it for covinience and save time.
+// 5.  here the user is objects i.e. real world entities which will use these classes and the task of programmer will be reduced drastically as she will not have to define attributes and behavior every time when a new object comes into existence.
+// 6. now n- number of objects can use this class for their convinience and save time.
 
 #include <iostream>
 using namespace std;
@@ -28,14 +28,13 @@ class Building
     }
 };
 
-//access modifiers :
-//1. public
-//2. private
-//3. protector
+// access modifiers :
+// 1. public
+// 2. private
+// 3. protector(Inheritance)
 int main()
 {
     Building b1;
     b1.door;
     b1.window;
-
 }
---
+++ Concepts/arrays.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+// Array stores multiple values of same datatype in a single variable
+//Arrays stores multiple values of same types of elements that have contigous memory location in a single variable
+
+int main(){
+    int var = 5;
+    int arr[]
+
+}
