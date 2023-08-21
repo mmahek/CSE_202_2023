c54d3cfa98bc079ba0416b17f025fb8191706756
Mahek <mmahekk24@gmail.com>
2023-08-21 21:00:27 +0530

    manipulators

---
+++ References and Pointers/manipulator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+//Manipulators
+//these are special functions that are used to change certain charactersticks of i/o and o/p
+//eg: endl, \n, 
+int main(){ 
+    int a = 123;
+
+    cout << setfill('') << setw(15) << a << endl;
+    return 0;
+}
