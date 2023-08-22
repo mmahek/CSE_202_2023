eff853e7c3812ca1c8fa1abca0709339fe5da956
Mahek <mmahekk24@gmail.com>
2023-08-22 20:47:49 +0530

    Pointers

---
+++ References and Pointers/pionters.cpp
@@ -0,0 +1,11 @@
+// pointers are variables that stores value of memory address of another variable. it is a hexadecimal value. 
+//pointer points at the memory block whose value it has stored in it.
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    int var = 10;
+     int *ptr;l //this is a pointer variable
+    return 0;
+}
