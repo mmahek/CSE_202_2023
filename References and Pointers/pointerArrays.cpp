4f3784be096684e91a7c3f37c1a13f23bd460ec8
Mahek <mmahekk24@gmail.com>
2023-08-29 19:41:43 +0530

    Arrays and pointers

--- References and Pointers/pointerArrays.cpp
+++ References and Pointers/pointerArrays.cpp
@@ -5,7 +5,7 @@ int main(){
     cout << "1 - " << arr << endl;//this points to the address of array , first value of array as well
     cout << "2 - " << arr[0] << endl;//this points to the first element or element at 0th index of array
     cout << "3 - " << &arr[0] << endl;//this points to the address of first element's address i.e. address of array, where it starts
-    cout <<"4 - " << *arr << endl;//dereferencing the value of array, the
+    cout << "4 - " << *arr << endl;//dereferencing the value of array, the
     cout << "5 - " << *arr+1 << endl;//prints 24 as arr pointer points to the first element of array and it is incremented by 1 thus 23 +1 = 24
     cout << "6 - " << *(arr + 2) << endl;// this points to the second element of array
     cout << "7 - " << *(arr) + 1 << endl;//prints 24
