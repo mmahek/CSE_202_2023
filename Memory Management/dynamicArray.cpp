0e786c0768b57dffe04676e6300cd2afe122b521
Mahek <mmahekk24@gmail.com>
2023-10-03 20:07:09 +0530

    Memory Management

---
+++ Memory Management/dynamicArray.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//this function returns the sum of all elements of array created 
+int getSum(int* arr, int n){
+    int sum = 0 ;
+    for( int  i = 0 ; i < n ; i++ ){
+            sum+= arr[i];
+    }
+    return sum;
+}
+int main(){
+    int n;
+    cout << "Enter the size of array: " ;
+    cin >> n;
+
+    // variable size array
+    int *arr = new int[n];
+    //memory is allocated at runtime
+
+    for(int i = 0 ; i < n ; i++){
+            cin >> arr[i];
+    }
+
+    int ans = getSum(arr, n);
+    cout << "Sum is: " << ans << endl;
+    return 0;
+}
